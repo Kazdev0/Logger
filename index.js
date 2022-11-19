0c91e06f3836ba75c81dffee00a6e92551952dd7
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-11-20 00:41:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-const color = require("colors");
-const fs = require("node:fs");
+const Kaz = require("./src/index");
+const Logger = new Kaz()
 
-class Logger {
-    constructor(){
-        
-    if(!fs.existsSync('log.txt')){
-            fs.writeFileSync('log.txt',' ');
-            console.log("Log.txt Bulunamadı.. Yenisi Oluşturuldu");
-        }
-    }
-    log(logger){
-        console.log(color.bold('[LOG]:'), color.blue(logger))
-        fs.appendFileSync('log.txt', `[LOG]: ${logger}\n`)
-    }
-    warn(logger){
-        console.warn(color.yellow('[WARN]:'), color.blue(logger))
-        fs.appendFileSync('log.txt', `[WARN]: ${logger}\n`)
-
-    }
-    error(logger) {
-        console.error(color.red('[ERROR]:'), color.blue(logger))
-        fs.appendFileSync('log.txt', `[ERROR]: ${logger}\n`)
-    }
-}
-
-module.exports = Logger;
+Logger.log("asdasdsdwqdasqwe2131a")
+Logger.error("asdasdsdwqdasqwe2131a")
+Logger.warn("asdasdsdwqdasqwe2131a")
